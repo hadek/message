cc1a76d07af2a5974a9222dc22947fab90a9b5c5
hadek <hadekfadel@gmail.com>
2016-10-26 09:17:59 +0200

    "fin insertion"

--- traitement.php
+++ traitement.php
@@ -3,19 +3,29 @@
 // connexion à la BDD
 include('connexion.php');
 //réception des données
-$nom = $POST['nom']
-$prenom = $POST['prenom'];
-$mail = $POST[''];
-$message = $POST[''];
+$nom = $_POST['nom'];
+$prenom = $_POST['prenom'];
+$mail = $_POST['email'];
+$date= date('Y-m-d');
+$message = $_POST['message'];
 //print_r($_POST);
 // préparation de la requete
-$req="INSERT INTO `messages`( `nom`, `prenom`,`mail`, `message`) VALUES (:nom, :prenom, :message)";
-$res = execute(array('nom'=>$nom,
+$req="INSERT INTO `messages`( `nom`, `prenom`,`mail`,`date_message`, `message`) VALUES (:nom, :prenom,:mail,:date_message, :message)";
+$res = $bdd->prepare($req);
+$erreur =$res->execute(array('nom'=>$nom,
 					  'prenom'=>$prenom,
 					  'mail'=>$mail,
+					  'date_message'=>$date,
 					  'message'=>$message));
-echo $res;
+echo $erreur;
 // Message en cas d' erreur
-
+//si $erreur = 1 alors pas d'erreur / renvoi à la page d'accueil 
+if($erreur==1){
+	header('Location:formulaire_contact.php');
+}
+//sinon affiche message d'erreur
+else{
+	echo 'désolé erreur ';
+} 
 
 ?>
